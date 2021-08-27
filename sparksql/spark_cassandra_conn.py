6818011a34296ec91933e0a71474b8ec5a5cfaca
The Learner <samarkant1@yahoo.com>
2021-08-27 10:29:42 +0530

    Update spark_cassandra_conn.py

--- sparksql/spark_cassandra_conn.py
+++ sparksql/spark_cassandra_conn.py
@@ -1,3 +1,4 @@
+# pyspark -packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.0 --conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions  --conf spark.cassandra.connection.host=localhost
 import os
 import sys
 from sys import argv
